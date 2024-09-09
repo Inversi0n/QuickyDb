6307ffcb29fefdf8fcd25ca186ab1c7851a10ca2
Mikhail <iammlyushakov@mail.ru>
2024-09-09 10:10:43 +0400

    incorrect line sql parsing template

---
+++ QuickyDb/QuickyTree/SqlParsing/SqlToExpression/SqlToExpressionConverter.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq.Expressions;
+
+namespace QuickyTree.SqlParsing.SqlToExpression
+{
+    public class Converter
+    {
+        public Expression[] Foo(string sql)
+        {
+            var lines = sql.Split(new char[] { '\n' }).Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
+
+            Expression curEx = null;
+            foreach (var line in lines)
+            {
+                var words = line.Split(new char[] { ',', ' ', '.' }).Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
+                foreach (var word in words)
+                {
+                    if (curEx == null)
+                    {
+                        if (word.ToLower() != "select")
+                            throw new Exception($"Expected Select, not {word}");
+
+                    }
+                }
+            }
+            return default;
+        }
+        private
+    }
+}
---
+++ QuickyDb/Tests/SqlTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class SqlTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+        }
+    }
+}
